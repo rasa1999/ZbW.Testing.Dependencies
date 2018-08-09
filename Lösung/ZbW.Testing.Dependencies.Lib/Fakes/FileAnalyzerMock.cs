cef837e9f7afc2611dcdcb7a0fd7f42df4d4c0a3
Michael Keiser <m.keiser@bst.ch>
2018-08-09 16:54:03 +0200

    Initial commit

---
+++ Lösung/ZbW.Testing.Dependencies.Lib/Fakes/FileAnalyzerMock.cs
@@ -0,0 +1,24 @@
+﻿namespace ZbW.Testing.Dependencies.Lib.Fakes
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    public class FileAnalyzerMock : IFileAnalyzer
+    {
+        public bool CheckFormatCalled { get; private set; }
+
+        public bool CheckPermissionsCalled { get; private set; }
+
+        public bool CheckFormat(string path)
+        {
+            CheckFormatCalled = true;
+            return true;
+        }
+
+        public bool CheckPermissions(string path)
+        {
+            CheckPermissionsCalled = true;
+            return true;
+        }
+    }
+}
---
+++ Lösung/ZbW.Testing.Dependencies.Lib/IFileAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿namespace ZbW.Testing.Dependencies.Lib
+{
+    public interface IFileAnalyzer
+    {
+        bool CheckFormat(string path);
+
+        bool CheckPermissions(string path);
+    }
+}
